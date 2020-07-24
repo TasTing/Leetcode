b86de66c7233379d72e32198f4c945e43aa94aed
Ting <badonlineqq@gmail.com>
2020-07-24 22:03:02 +1000

    88
    first commit

---
+++ Problems/88-Merge Sorted Array.js
@@ -0,0 +1,15 @@
+let nums1 = [1,2,3,0,0,0]
+let m = 3
+let nums2 = [2,5,6]
+let n = 3
+
+
+
+
+nums1.splice(m,n,)
+nums1 = nums1.concat(nums2)
+nums1 = nums1.sort()
+
+console.log(nums1)
+
+// only available when there is no limit of storage use
